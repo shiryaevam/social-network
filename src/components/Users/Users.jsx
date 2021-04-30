b123250b43604ff18105fc07161f139d9e98d787
Aleksey.Shiryaev <Aleksey.Shiryaev@payever.org>
2021-04-30 10:51:35 +0400

    feat: add axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,49 +1,16 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from "axios";
+import photoUser from '../../assets/image/avatarka.png'
 
 let Users = (props) => {
-    if(props.users.length === 0){
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl:
-                    "https://sites.google.com/site/informaticschildren/_/rsrc/1333892088085/aleesa-popovi/%D0%90%D0%BB%D1%91%D1%88%D0%B0_%D0%9F%D0%BE%D0%BF%D0%BE%D0%B2%D0%B8%D1%87.JPG",
-                fullName: "Aleshka",
-                status: "I am boss",
-                followed: true,
-                location: { city: "Samara", country: "Russia" },
-            },
-            {
-                id: 2,
-                photoUrl:
-                    "https://sites.google.com/site/informaticschildren/_/rsrc/1333892088085/aleesa-popovi/%D0%90%D0%BB%D1%91%D1%88%D0%B0_%D0%9F%D0%BE%D0%BF%D0%BE%D0%B2%D0%B8%D1%87.JPG",
-                fullName: "Sergey",
-                status: "NO! I am boss",
-                followed: true,
-                location: { city: "Samara", country: "Russia" },
-            },
-            {
-                id: 3,
-                photoUrl:
-                    "https://sites.google.com/site/informaticschildren/_/rsrc/1333892088085/aleesa-popovi/%D0%90%D0%BB%D1%91%D1%88%D0%B0_%D0%9F%D0%BE%D0%BF%D0%BE%D0%B2%D0%B8%D1%87.JPG",
-                fullName: "Antoshka",
-                status: "NO!! NO!! I am boss",
-                followed: false,
-                location: { city: "Sankt-Peterburg", country: "Russia" },
-            },
-            {
-                id: 4,
-                photoUrl:
-                    "https://sites.google.com/site/informaticschildren/_/rsrc/1333892088085/aleesa-popovi/%D0%90%D0%BB%D1%91%D1%88%D0%B0_%D0%9F%D0%BE%D0%BF%D0%BE%D0%B2%D0%B8%D1%87.JPG",
-                fullName: "Andrey",
-                status: "I am fish",
-                followed: false,
-                location: { city: "Saransk", country: "Russia" },
-            },
-        ]);
-    }
+  if (props.users.length === 0) {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+    .then(response => {
+        props.setUsers(response.data.items)
+    })
 
-
+     }
 
   return (
     <div>
@@ -51,7 +18,7 @@ let Users = (props) => {
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} className={styles.usersPhoto} />
+              <img src={u.photos.small != null ? u.photos.small: photoUser} className={styles.usersPhoto} />
             </div>
             <div>
               {u.followed ? (
@@ -75,12 +42,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
             </span>
           </span>
         </div>
