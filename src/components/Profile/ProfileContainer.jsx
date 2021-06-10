bd7501e7694ed0ec25df7a8d1873940dfb85bac7
Aleksey <shiryaev.am@mail.ru>
2021-06-10 18:11:48 +0400

    fix: setUserProfile

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -12,7 +12,7 @@ class ProfileContainer extends React.Component {
       userId = 2;
     }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2` + userId)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
